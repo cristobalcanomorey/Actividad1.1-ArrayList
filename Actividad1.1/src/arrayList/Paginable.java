769997a9e772a2c4979e96eb2913814340374835
Tofol <tofolcanodaw2@gmail.com>
2019-10-05 22:04:35 +0200

    Get Contains CompareTo Y Equals

--- Actividad1.1/src/arrayList/Paginable.java
+++ Actividad1.1/src/arrayList/Paginable.java
@@ -2,6 +2,7 @@ package arrayList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import main.Producto;
 
@@ -15,12 +16,12 @@ public class Paginable <E extends Producto> {
 	 * int getTotalPages()
 	 * size*/
 	
-	private ArrayList<Object> productos = new ArrayList<Object>();
+	private ArrayList<Producto<?>> productos = new ArrayList<Producto<?>>();
 	private int prodPorPag = 3;
 	private int numDePags = 0;
 	
-	Paginable(Producto[] prod){
-		this.productos = new ArrayList<Object>(Arrays.asList(prod));
+	Paginable(Producto<?>[] prod){
+		this.productos = new ArrayList<Producto<?>>(Arrays.asList(prod));
 		numDePags = (int) Math.ceil(productos.size()/prodPorPag);
 	}
 		
@@ -28,7 +29,7 @@ public class Paginable <E extends Producto> {
 	 * Solo añade productos en el array
 	 * @param p Producto
 	 */
-	public void add(Producto p){
+	public void add(Producto<?> p){
 		productos.add(p);
 		numDePags = (int) Math.ceil(productos.size()/prodPorPag);
 	}
@@ -37,7 +38,7 @@ public class Paginable <E extends Producto> {
 	 * Elimina el producto del array
 	 * @param p Producto
 	 */
-	public void remove(Producto p) {
+	public void remove(Producto<?> p) {
 		if(contains(p)) {
 			productos.remove(p);
 		}
@@ -50,8 +51,17 @@ public class Paginable <E extends Producto> {
 	 * @param p Producto
 	 * @return True si p está en el array
 	 */
-	public boolean contains(Producto p) {
-		return productos.contains(p);
+	public boolean contains(Producto<?> producto) {
+		boolean resul = false;
+		for (Producto<?> p : productos) {
+			if(resul) {
+				break;
+			} else {
+				resul = producto.equals(p);
+			}
+			
+		}
+		return resul;
 	}
 	
 	/***
@@ -59,10 +69,15 @@ public class Paginable <E extends Producto> {
 	 * @param n Número de página
 	 * @return Devuelve array de productos
 	 */
-	public Producto[] getPage(int n) {
-		Producto[] resul = null;
-		// primer prod = n * numDePags
-		
+	public Producto<?>[] getPage(int n) {
+		Producto<?>[] resul = new Producto<?>[prodPorPag];
+		int primProd = n * prodPorPag;
+		int ultProd = primProd+prodPorPag;
+		if(ultProd>productos.size()) {
+			ultProd = productos.size();
+		}
+		List<Producto<?>> pag = productos.subList(primProd, ultProd);
+		resul = (Producto<?>[]) pag.toArray();
 		
 		return resul;
 	}
--- Actividad1.1/src/main/Producto.java
+++ Actividad1.1/src/main/Producto.java
@@ -35,16 +35,28 @@ public class Producto<E> implements Comparable<Object>{	//implementar comparable
 	public void setPreu(Double preu) {
 		this.preu = preu;
 	}
+	
+	/***
+	 * Si son iguales devuelve true, si no false. 
+	 */
+	@Override
+	public boolean equals(Object arg0) {
+		Producto<?> p = (Producto<?>)arg0;
+		return this.id == p.id;
+	}
 
 	/***
-	 * Si son iguales devuelve 1, si no -1
+	 * Si son iguales devuelve 0, si el del parámetro es menor -1 si es mayor 1
 	 */
 	@Override
 	public int compareTo(Object arg0) {
-		if(this.equals(arg0)) {
+		Producto<?> p = (Producto<?>)arg0;
+		if(this.id < p.id) {
 			return 1;
-		} else {
+		} else if(this.id > p.id){
 			return -1;
+		} else {
+			return 0;
 		}
 	}
 	
